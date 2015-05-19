d65a1247db669dd27a2cfe1b2717b62a68e55fb2
Ray Canzanese <rcanzanese@gmail.com>
2015-05-19 13:16:59 -0400

    Added resx files

---
+++ ProjectInstaller.Designer.cs
@@ -0,0 +1,87 @@
+﻿/* Copyright 2014 Ray Canzanese
+ * email:  rcanzanese@gmail.com
+ * url:    www.canzanese.com 
+ *
+ * This file is part of SystemCallService.
+ *
+ * SystemCallService is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * SystemCallService is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SystemCallService.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace SystemCallService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            this.serviceProcessInstaller1.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceProcessInstaller1_AfterInstall);
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Collects system call traces for research purposes.  Contact Ray Canzanese -- rcanzanese@gmail.com";
+            this.serviceInstaller1.DisplayName = "System Call Service";
+            this.serviceInstaller1.ServiceName = "SystemCallService";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            this.serviceInstaller1.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceInstaller1_AfterInstall);
+            this.serviceInstaller1.ServicesDependedOn = new string[] {"LanmanServer"};
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private ServiceProcessInstaller serviceProcessInstaller1;
+        private ServiceInstaller serviceInstaller1;
+    }
+}
---
+++ ProjectInstaller.cs
@@ -0,0 +1,44 @@
+﻿/* Copyright 2014 Ray Canzanese
+ * email:  rcanzanese@gmail.com
+ * url:    www.canzanese.com 
+ *
+ * This file is part of SystemCallService.
+ *
+ * SystemCallService is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * SystemCallService is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SystemCallService.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace SystemCallService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
